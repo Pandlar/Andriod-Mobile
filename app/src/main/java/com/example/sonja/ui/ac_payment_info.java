0f1762f76199693d13448ed424a73cf6fa6fc4b3
sebb92 <38911160+sebb92@users.noreply.github.com>
2018-05-30 16:10:33 +0200

    first commit profile

---
+++ app/src/main/java/com/example/sonja/ui/ac_payment_info.java
@@ -0,0 +1,13 @@
+package com.example.sonja.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ac_payment_info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ac_payment_info);
+    }
+}
---
+++ app/src/main/java/com/example/sonja/ui/account_01.java
@@ -0,0 +1,13 @@
+package com.example.sonja.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class account_01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_01);
+    }
+}

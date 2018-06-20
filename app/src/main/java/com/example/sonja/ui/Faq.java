18ea1243f90ab074b85208e8d93822c2a9bb2e3f
sebb92 <38911160+sebb92@users.noreply.github.com>
2018-06-20 15:35:25 +0200

    neu

---
+++ app/src/main/java/com/example/sonja/ui/Faq.java
@@ -0,0 +1,30 @@
+package com.example.sonja.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Faq extends AppCompatActivity implements View.OnClickListener{
+
+    Button faq_back_to_settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+
+        faq_back_to_settings = findViewById(R.id.faq_back_settings);
+        faq_back_to_settings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(Faq.this, AccountSettings03.class);
+        startActivity(intent);
+        this.finish();
+
+    }
+}

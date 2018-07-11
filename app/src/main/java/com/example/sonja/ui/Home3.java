86e9629ff2cdff26296de1ce4c1f3f45015a6c2f
sonni93 <38693058+sonni93@users.noreply.github.com>
2018-07-11 17:50:47 +0200

    home screens text aus java class für db anbindung

--- app/src/main/java/com/example/sonja/ui/Home3.java
+++ app/src/main/java/com/example/sonja/ui/Home3.java
@@ -22,10 +22,20 @@ public class Home3 extends AppCompatActivity implements View.OnClickListener{
     TextView noch_offen2;
     TextView Fahrt_abgesagt2;
     Button kasten_chronik;
+
+    TextView textView_Ankunft_Ort;
+    TextView textView_Abfahrt_Ort;
+    TextView textView_Freie_Sitzplaetze;
+    TextView textView_Anzahl_Freie_Sitzplaetze;
+    TextView textView_Fahrer;
+    TextView textView_Uhrzeit;
+
     public static int status_fahrer = 2; //0: noch offen 1: bestätigen 2: bestätigt 3: Fahrt abgesagt
     public static int status_mitfahrer = 2; //0: noch offen 1: bestätigen 2: bestätigt 3: Fahrt abgesagt
 
-
+    /**
+     * initialisiert die Button Navigation Bar
+     */
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener(){
 
@@ -87,7 +97,28 @@ public class Home3 extends AppCompatActivity implements View.OnClickListener{
 
         nachStatusAnzeigen_Fahrer();
         nachStatusAnzeigen_Mitfahrer();
+
+        //TODO  Text aus DB in Textviews einfügen
+        textView_Fahrer = (TextView) findViewById(R.id.textView_Fahrer);
+        textView_Fahrer.setText("Du bist Fahrer!");
+
+        textView_Uhrzeit = (TextView) findViewById(R.id.textView_Uhrzeit);
+        textView_Uhrzeit.setText("08. Juni 08:00 Uhr");
+
+        textView_Ankunft_Ort = (TextView) findViewById(R.id.textView_Ankunft_Ort);
+        textView_Ankunft_Ort.setText("Badensche Str. 50-51, 10715 Berlin");
+
+        textView_Abfahrt_Ort = (TextView) findViewById(R.id.textView_Abfahrt_Ort);
+        textView_Abfahrt_Ort.setText("Berliner Str. 30, 10715 Berlin");
+
+        textView_Freie_Sitzplaetze = (TextView) findViewById(R.id.textView_Freie_Sitzplaetze);
+        textView_Freie_Sitzplaetze.setText("Freie Sitzplätze: ");
+
+        textView_Anzahl_Freie_Sitzplaetze = (TextView) findViewById(R.id.textView_Anzahl_Freie_Sitzplaetze);
+        textView_Anzahl_Freie_Sitzplaetze.setText("1");
+
     }
+
 
     public void onClick(View v) {
         switch (v.getId()) {
@@ -108,6 +139,9 @@ public class Home3 extends AppCompatActivity implements View.OnClickListener{
         }
     }
 
+    /**
+     * zeigt den Status der Fahrt an für Fahrer(noch offen, bestätigt, noch bestätigen, Fahrt abgesagt)
+     */
     public void nachStatusAnzeigen_Fahrer(){
         switch(status_fahrer){
             case 0:
@@ -137,6 +171,9 @@ public class Home3 extends AppCompatActivity implements View.OnClickListener{
 
         }}
 
+    /**
+     * zeigt den Status der Fahrt an für Mitfahrer(noch offen, bestätigt, jetzt bestätigen, Fahrt abgesagt)
+     */
     public void nachStatusAnzeigen_Mitfahrer(){
         switch(status_mitfahrer){
             case 0:
--- app/src/main/java/com/example/sonja/ui/Home4.java
+++ app/src/main/java/com/example/sonja/ui/Home4.java
@@ -23,6 +23,13 @@ public class Home4 extends AppCompatActivity implements View.OnClickListener{
     public static int status_fahrer = 0; //0: bewerten 1: bewertet 2: Fahrt abgesagt
     public static int status_mitfahrer = 0; //0: bewerten 1: bewertet 2: Fahrt abgesagt
 
+    TextView textView_Ankunft_Ort;
+    TextView textView_Abfahrt_Ort;
+    TextView textView_Freie_Sitzplaetze;
+    TextView textView_Anzahl_Freie_Sitzplaetze;
+    TextView textView_Fahrer;
+    TextView textView_Uhrzeit;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener(){
@@ -79,6 +86,26 @@ public class Home4 extends AppCompatActivity implements View.OnClickListener{
 
         nachStatusAnzeigen_Fahrer();
         nachStatusAnzeigen_Mitfahrer();
+
+        //TODO  Text aus DB in Textviews einfügen
+        textView_Fahrer = (TextView) findViewById(R.id.textView_Fahrer);
+        textView_Fahrer.setText("Du bist Fahrer!");
+
+        textView_Uhrzeit = (TextView) findViewById(R.id.textView_Uhrzeit);
+        textView_Uhrzeit.setText("08. Juni 08:00 Uhr");
+
+        textView_Ankunft_Ort = (TextView) findViewById(R.id.textView_Ankunft_Ort);
+        textView_Ankunft_Ort.setText("Badensche Str. 50-51, 10715 Berlin");
+
+        textView_Abfahrt_Ort = (TextView) findViewById(R.id.textView_Abfahrt_Ort);
+        textView_Abfahrt_Ort.setText("Berliner Str. 30, 10715 Berlin");
+
+        textView_Freie_Sitzplaetze = (TextView) findViewById(R.id.textView_Freie_Sitzplaetze);
+        textView_Freie_Sitzplaetze.setText("Freie Sitzplätze: ");
+
+        textView_Anzahl_Freie_Sitzplaetze = (TextView) findViewById(R.id.textView_Anzahl_Freie_Sitzplaetze);
+        textView_Anzahl_Freie_Sitzplaetze.setText("1");
+
 
     }
     public void onClick(View v) {

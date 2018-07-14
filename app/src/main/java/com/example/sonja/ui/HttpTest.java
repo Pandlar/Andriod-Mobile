b627ce8a4b4c80088d169831cb151677b6fc2f44
Sarah <Sarah.gosten@web.de>
2018-07-14 12:28:08 +0200

    Merge DB-Verbindung und Master korrigiert

--- app/src/main/java/com/example/sonja/ui/HttpTest.java
+++ app/src/main/java/com/example/sonja/ui/HttpTest.java
@@ -160,6 +160,50 @@ public class HttpTest {
 
     }
 
+    // HTTP POST request
+    public void sendPostSignUp(String table, String ratingText, String createdBy, String ratedUserId, String matchID, int stars) throws Exception {
+
+        String url = urlip + table;
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        //add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        /*String urlParameters = "id=" + id + "&ratingText=" + ratingText+ "&createdBy=" + createdBy + "&ratedUserId=" +
+                ratedUserId + "&matchId=" + matchID + "&stars=" + stars;*/
+
+        String urlParameters = "ratingText=" + ratingText+ "&createdBy=" + createdBy + "&ratedUserId=" +
+                ratedUserId + "&matchId=" + matchID + "&stars=" + stars;
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
     public void sendPut() throws Exception {
 
         String id = "2";
